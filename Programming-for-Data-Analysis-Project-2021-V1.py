2377ab905f51774a5516edf3c7e57fc3a7f0bc51
G00398318@gmit.ie <G00398318@gmit.ie>
2021-12-09 00:41:31 +0000

    Check dataset in pandas

--- Programming-for-Data-Analysis-Project-2021-V1.py
+++ Programming-for-Data-Analysis-Project-2021-V1.py
@@ -12,15 +12,29 @@
 # All imports are listed in the requirments.txt
 ########################################################################
 
+
 # Imports
 
 import pandas as pd
+import matplotlib.pyplot as plt
 import warnings
+
 warnings.filterwarnings('ignore')
+
 
 
 # import data from data folder
 # https://github.com/TarekDib03/Analytics/blob/master/Week3%20-%20Logistic%20Regression/Data/framingham.csv
 df = pd.read_csv("data/framingham.csv")
+
+# Print first 5 entries
 print(df.head(5))
 
+# Print the count, mean, std, min, max as well as lower, 50 and upper percentiles. 
+# The lower (25) and upper (75) percentiles. The 50 percentile is the same as the median.
+# This is to give an overview of the dataset including missing values
+print(df.describe())
+
+# Plot a histogram of Age
+df.hist(column='age')
+plt.show()
